1e494406287f91c7865d416b6f8aefb432a113e2
Sital Chandra <sital24chandra@gmail.com>
2020-09-30 16:34:16 +0530

    First Commit

--- sketch.js
+++ sketch.js
@@ -14,17 +14,17 @@ function setup() {
 }
 
 function draw() {
-  background(0,0,0);  
+  background(0,0,0); 
 
-  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
-      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) {
-    movingRect.velocityX = movingRect.velocityX * (-1);
-    fixedRect.velocityX = fixedRect.velocityX * (-1);
+  // bounceOff(movingRect,fixedRect);
+
+  if(isTouching(movingRect,fixedRect)){
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red"; 
+  } else {
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
   }
-  if (movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
-    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
-    movingRect.velocityY = movingRect.velocityY * (-1);
-    fixedRect.velocityY = fixedRect.velocityY * (-1);
-  }
+
   drawSprites();
 }
